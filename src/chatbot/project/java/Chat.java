a6f67833a0f56ec65bf498d4097bd075a82ecc04
nidhigul <31624956+nidhigul@users.noreply.github.com>
2023-10-12 12:45:40 +0530

    Update Chat.java

--- src/chatbot/project/java/Chat.java
+++ src/chatbot/project/java/Chat.java
@@ -2,98 +2,98 @@ package chatbot.project.java;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+
 import com.mysql.cj.xdevapi.Statement;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.Collator;
 
 
 public class Chat {
 	Chat(){
-		// Creat einterface.
-		final JFrame f1= new JFrame("CHAT HERE"); 
-		f1.setExtendedState(f1.MAXIMIZED_BOTH);
-		JPanel panel=new JPanel(); 
-		JButton b=new JButton(new ImageIcon("C:\\Users\\rama bebs\\Downloads\\Java Module\\buttton.JPG"));
-		panel.setBackground(Color.GRAY);
-		final JTextField ta = new JTextField("Ask me anything...",100);
-		Border border = BorderFactory.createLineBorder(Color.black);
-	    ta.setBorder(BorderFactory.createCompoundBorder(border,
-	            BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-		
+		final JFrame frame= new JFrame("CHAT HERE"); 
+		frame.setExtendedState(frame.MAXIMIZED_BOTH);
 		final ImageIcon imageIcon = new ImageIcon("C:\\\\Users\\\\rama bebs\\\\Downloads\\\\Java Module\\\\bot.jpg");
-		JTextArea disp = new JTextArea(){
+		JPanel panel=new JPanel(){
 		      Image image = imageIcon.getImage();
 		      {
-		        setOpaque(false);
+		        setOpaque(false); 
 		      }
-
 		      public void paint(Graphics g) {
-		        g.drawImage(image, 0, 0, this);
+		    	int x = (this.getWidth() - image.getWidth(null))/2;
+		    	int y = (this.getHeight() - image.getHeight(null))/2;
+		        g.drawImage(image, x, y, this);
 		        super.paint(g);
 		      }
-		    };
-		disp.setForeground(Color.white);
-		disp.setRows(30);
-		disp.setColumns(100);
-		disp.setBorder(BorderFactory.createCompoundBorder(border,
+		    }; 
+		JButton button=new JButton(new ImageIcon("C:\\Users\\rama bebs\\Downloads\\Java Module\\buttton.JPG"));
+		panel.setBackground(Color.getHSBColor(135, 206, 250));
+		final JTextField questionField = new JTextField("Ask me anything...",100);
+		Border border = BorderFactory.createLineBorder(Color.black);
+		questionField.setBorder(BorderFactory.createCompoundBorder(border,
 	            BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-		disp.setEditable(false);
-		disp.setFont(new Font("Monospaced",Font.PLAIN,15));
-		disp.setText("Welcome, my name is blue and i will be your friend. let's get to know each other");
-		b.setBounds(1200,580,150,58);
-		f1.add(b); 
-		panel.add(disp);
-		panel.add(ta);
-		f1.add(panel);
-		f1.setVisible(true); 
+		final JTextArea display = new JTextArea();
+		display.setOpaque(false);
+		questionField.setOpaque(false);
+		display.setRows(30);
+		display.setColumns(100);
+		display.setBorder(BorderFactory.createCompoundBorder(border,
+	            BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+		display.setEditable(false);
+		display.setFont(new Font("Monospaced",Font.PLAIN,15));
+		display.setText("Welcome, my name is blue and i will be your friend. let's get to know each other");
+		button.setBounds(1200,580,150,58);
+		frame.add(button); 
+		panel.add(display);
+		panel.add(questionField);
+		frame.add(panel);
+		frame.setVisible(true); 
 
-		//To establish a connection with MySql using jdbc
-		b.addActionListener(new ActionListener() {
+
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				try {
 					String url = "jdbc:mysql://localhost:3306/chatbot";
 					String username = "root";
-					String password = "blueheart";
-
-					Class.forName("com.mysql.jdbc.Driver"); 
+					String password = "blueheart"; 
 					Connection con=DriverManager.getConnection(  
 							url,username,password);  
-					
+					String questions = questionField.getText();
 					java.sql.Statement st = con.createStatement();
-		            String sql = "Select * from chats";
-		           
-		           ResultSet rs = st.executeQuery(sql);
-		           
-		           while(rs.next()) {
-		        	   String questions = ta.getText();
-		                    String ques = rs.getString("question");
-		                    if(ques.trim().equals(questions.trim()))
+					String query = "SELECT answer FROM chats WHERE question = ?";
+		            PreparedStatement preparedStatement = con.prepareStatement(query);
+		            preparedStatement.setString(1, questions);
+		            ResultSet rs = preparedStatement.executeQuery();
+		            if(rs.next())
 		                    {
 		                    	String ans = rs.getString("answer");
-								disp.setText("User -> "+questions + "\n"+"Blue -> "+ans);
+								display.setText("User -> "+questions + "\n"+"Blue -> "+ans);
 		                    }
-					    // To call google in case the question is not in the database
 		                    else {
-		                    	disp.setText("Sorry, I don't know the answer to this question, "
-		                    			+ "but here's something from where you can get it");
-		                    	String googleUrl = "https://www.google.com";
+		                    	display.setText("I apologize for not knowing the answer to this, "
+		                    			+ "however here is a source where you can find it.\r\n");
+		                    	String googleUrl = "https://www.google.com?q="+URLEncoder.encode(questions,"UTF-8");	
 		                    	try {
 		                                Desktop desktop = Desktop.getDesktop();
 		                                URI uri = new URI(googleUrl);
 		                                desktop.browse(uri);
-		                                break;
 		                        } catch (Exception e) {
 		                            e.printStackTrace();
 		                        }
 		                    }
-		                   }
 					st.close();
 					con.close();
 				}
@@ -102,7 +102,7 @@ public class Chat {
 			}
 			
 		});
-		f1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 	}
 	
 }
